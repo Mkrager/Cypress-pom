0c976b626dc40fd4497e6050df5c3d8b0bc97079
Smaga Max <smaga.max@gmail.com>
2026-01-20 16:01:42 +0200

    fix .env not see bug

--- configs.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: `${process.env.BASE_URL}`,
-  },
-});
--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const { defineConfig } = require("cypress");
+
+console.log("BASE_URL:", process.env.BASE_URL);
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: process.env.BASE_URL || "https://www.redmine.org",
+    chromeWebSecurity: false,
+  },
+  env: {
+    VALID_USER_LOGIN: process.env.VALID_USER_LOGIN,
+    VALID_USER_PASSWORD: process.env.VALID_USER_PASSWORD,
   },
 });
